8aa33cfd542548c144cdb1fc863777f4de8abf21
Jonas Dann <jonas.dann@inf.ethz.ch>
2025-11-22 15:58:31 +0100

    Fixed AXI Demultiplexer issue

--- src/hdl/axi/axi_demultiplexer.sv
+++ src/hdl/axi/axi_demultiplexer.sv
@@ -12,22 +12,51 @@ module AXIDemultiplexer #(
     AXI4S.m out[NUM_STREAMS]
 );
 
-logic selected_ready;
-logic[NUM_STREAMS - 1:0] selected, out_ready;
+typedef logic[$clog2(NUM_STREAMS) - 1:0] select_t;
 
-assign selected_ready = |(selected & out_ready);
-assign select.ready = in.tvalid && in.tlast && selected_ready;
+// If we don't pull this into an internal register we have to assign valid to ready which is bad
+select_t select_reg; 
+logic    select_reg_valid;
 
-assign in.tready = select.valid && selected_ready;
+logic selected_stream_ready;
+logic[NUM_STREAMS - 1:0] is_selected, stream_ready;
+logic was_last_data_beat;
+
+always_ff @(posedge clk) begin
+    if (rst_n == 1'b0) begin
+        select_reg_valid <= 1'b0;
+    end else begin
+        if (select.valid) begin
+            if (select.ready) begin
+                select_reg       <= select.data;
+                select_reg_valid <= 1'b1;
+            end
+        end else begin
+            select_reg <= select_reg;
+
+            if (was_last_data_beat) begin
+                select_reg_valid <= '0;
+            end else begin
+                select_reg_valid <= select_reg_valid;
+            end
+        end
+    end
+end
+
+assign selected_stream_ready = select_reg_valid && |(is_selected & stream_ready);
+assign was_last_data_beat    = in.tvalid && in.tlast && selected_stream_ready;
+assign select.ready = !select_reg_valid || was_last_data_beat;
+
+assign in.tready = selected_stream_ready;
 
 for (genvar I = 0; I < NUM_STREAMS; I++) begin
-    assign selected[I] = I == select.data;
-    assign out_ready[I] = out[I].tready;
+    assign is_selected[I]  = I == select_reg;
+    assign stream_ready[I] = out[I].tready;
 
-    assign out[I].tdata = in.tdata;
-    assign out[I].tkeep = in.tkeep;
-    assign out[I].tlast = in.tlast;
-    assign out[I].tvalid = in.tvalid && select.valid && selected[I];
+    assign out[I].tdata  = in.tdata;
+    assign out[I].tkeep  = in.tkeep;
+    assign out[I].tlast  = in.tlast;
+    assign out[I].tvalid = in.tvalid && select_reg_valid && is_selected[I];
 end
 
 endmodule
