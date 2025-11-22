724310293ba182a1dd3ea57ec11b75e488b04460
Jonas Dann <jonas.dann@inf.ethz.ch>
2025-11-22 14:57:35 +0100

    Fixed masking in config FIFO

--- src/hdl/config/registers/config_write_fifo.sv
+++ src/hdl/config/registers/config_write_fifo.sv
@@ -12,12 +12,16 @@ module ConfigWriteFIFO #(
     ready_valid_i.m data // #(data_t)
 );
 
+logic valid;
+
+assign valid = conf.valid && conf.addr == ADDR;
+
 FIFO #(DEPTH, $bits(data_t)) inst_fifo (
     .i_clk(clk),
     .i_rst_n(rst_n),
 
     .i_data(data_t'(conf.data)),
-    .i_valid(conf.valid),
+    .i_valid(valid),
     .i_ready(),
 
     .o_data(data.data),
