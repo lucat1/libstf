aabf767d7d52b9f8648576de2383660d38bbe10e
Jonas Dann <jonas.dann@inf.ethz.ch>
2025-11-27 15:47:42 +0100

    Fixed bug in tagged multiplexer. Had to move all last handling back into second stage

--- src/hdl/crossbar/tagged_multiplexer.sv
+++ src/hdl/crossbar/tagged_multiplexer.sv
@@ -25,84 +25,50 @@ module TaggedMultiplexer #(
 localparam WAIT_ALL = 0;
 localparam FORWARD = 1;
 
-// -- 1. stage: Extract matches to simplify multiplexing logic -------------------------------------
-logic[NUM_INPUTS - 1:0] in_last, in_valid, in_ready;
-logic[NUM_INPUTS - 1:0] last_seen_1, n_last_seen_1;
+// -- 1. stage: Tag matching to simplify multiplexing logic slightly -------------------------------
 logic[NUM_INPUTS - 1:0] tag_matches;
-tagged_i #(data_t, 1) filtered[NUM_INPUTS](), internal_1[NUM_INPUTS]();
-
-always_ff @(posedge clk) begin
-    if (!reset_synced) begin
-        last_seen_1 <= '0;
-    end else begin
-        last_seen_1 <= n_last_seen_1;
-    end
-end
-
-always_comb begin
-    n_last_seen_1 = last_seen_1;
-
-    if (LAST_HANDLING == WAIT_ALL) begin
-        for (int i = 0; i < NUM_INPUTS; i++) begin
-            if (in_valid[i] && in_last[i] && (!tag_matches[i] || in_ready[i])) begin
-                n_last_seen_1[i] = 1'b1;
-            end
-        end
-        // Note: We need the one hot expression here too because otherwise the first and second 
-        // phase get out of sync
-        if (&last_seen_1 || ($onehot(in_valid & tag_matches) && |(in_valid & tag_matches & in_ready) && 
-                &(last_seen_1 | (in_valid & in_last)))) begin
-            n_last_seen_1 = '0;
-        end
-    end
-end
+tagged_i #(data_t, 1) matched[NUM_INPUTS](), mux_in[NUM_INPUTS]();
 
 for (genvar I = 0; I < NUM_INPUTS; I++) begin
-    assign in_last[I]  = in[I].last;
-    assign in_valid[I] = in[I].valid;
-    assign in_ready[I] = in[I].ready;
-
     assign tag_matches[I] = in[I].tag == ID;
 
     always_comb begin
         in[I].ready = 1'b1;
 
-        filtered[I].data  = in[I].data;
-        filtered[I].tag   = tag_matches[I];
-        filtered[I].keep  = in[I].keep;
-        filtered[I].last  = in[I].last;
-        filtered[I].valid = last_seen_1[I] ? 1'b0 : in[I].valid;
+        matched[I].data  = in[I].data;
+        matched[I].tag   = tag_matches[I];
+        matched[I].keep  = in[I].keep;
+        matched[I].last  = in[I].last;
+        matched[I].valid = in[I].valid;
 
-        // If we wait for all last signals, the inputs where we have seen the last belong to the next 
-        // stream and cannot yet be consumed
-        if ((LAST_HANDLING == WAIT_ALL && last_seen_1[I]) || (tag_matches[I] && !filtered[I].ready)) begin
+        if (((LAST_HANDLING == WAIT_ALL && in[I].last) || tag_matches[I]) && !matched[I].ready) begin
             in[I].ready = 1'b0;
         end
     end
 
-    TaggedSkidBuffer #(data_t, 1) inst_internal_buffer (.clk(clk), .rst_n(reset_synced), .in(filtered[I]), .out(internal_1[I]));
+    TaggedSkidBuffer #(data_t, 1) inst_internal_buffer (.clk(clk), .rst_n(reset_synced), .in(matched[I]), .out(mux_in[I]));
 end
 
 // -- 2. stage: Multiplex round robin --------------------------------------------------------------
 logic[NUM_INPUTS - 1:0] tags, is_selected;
 data_i #(data_t) selected();
 
-data_t internal_1_data[NUM_INPUTS];
-logic[NUM_INPUTS - 1:0] internal_1_keep, internal_1_last, internal_1_valid, internal_1_ready;
+data_t mux_in_data[NUM_INPUTS];
+logic[NUM_INPUTS - 1:0] mux_in_keep, mux_in_last, mux_in_valid, mux_in_ready;
 
-logic[NUM_INPUTS - 1:0] last_seen_2, n_last_seen_2;
+logic[NUM_INPUTS - 1:0] last_seen, n_last_seen;
 
-data_i #(data_t) internal_2(), n_internal_2();
+data_i #(data_t) mux_out(), n_mux_out();
 
 for(genvar I = 0; I < NUM_INPUTS; I++) begin
-    assign internal_1[I].ready = internal_1_ready[I]; // We need to reassign these values to local arrays because SystemVerilog thinks the iterator in a process is not constant for arrays of interfaces
+    assign mux_in[I].ready = mux_in_ready[I]; // We need to reassign these values to local arrays because SystemVerilog thinks the iterator in a process is not constant for arrays of interfaces
 
-    assign internal_1_data[I]  = internal_1[I].data;
-    assign internal_1_keep[I]  = internal_1[I].keep;
-    assign internal_1_last[I]  = internal_1[I].last;
-    assign internal_1_valid[I] = internal_1[I].valid;
+    assign mux_in_data[I]  = mux_in[I].data;
+    assign mux_in_keep[I]  = mux_in[I].keep;
+    assign mux_in_last[I]  = mux_in[I].last;
+    assign mux_in_valid[I] = mux_in[I].valid;
 
-    assign tags[I] = internal_1[I].valid && (!FILTER_KEEP || internal_1[I].keep) && internal_1[I].tag;
+    assign tags[I] = mux_in[I].valid && (!FILTER_KEEP || mux_in[I].keep) && mux_in[I].tag && !last_seen[I];
 end
 
 // -- Selection logic ------------------------------------------------------------------------------
@@ -150,78 +116,84 @@ always_comb begin
     selected.data  = '0;
     selected.keep  = 1'b0;
     selected.last  = 1'b0;
-    selected.valid = |is_selected; 
+    selected.valid = |tags; 
 
     for (int i = 0; i < NUM_INPUTS; i++) begin
-        selected.data |= is_selected[i] ? internal_1_data[i]  : '0;
-        selected.keep |= is_selected[i] ? internal_1_keep[i]  : 1'b0;
-        selected.last |= LAST_HANDLING == FORWARD && is_selected[i] ? internal_1_last[i] : 1'b0;
+        selected.data |= is_selected[i] ? mux_in_data[i]  : '0;
+        selected.keep |= is_selected[i] ? mux_in_keep[i]  : 1'b0;
+        selected.last |= LAST_HANDLING == FORWARD && is_selected[i] ? mux_in_last[i] : 1'b0;
     end
 end
 
 // -- Output logic ---------------------------------------------------------------------------------
 always_ff @(posedge clk) begin
     if (!reset_synced) begin
-        last_seen_2      <= '0;
-        internal_2.valid <= '0;
+        last_seen     <= '0;
+        mux_out.valid <= '0;
     end else begin
-        last_seen_2 <= n_last_seen_2;
+        last_seen <= n_last_seen;
 
-        internal_2.data  <= n_internal_2.data;
-        internal_2.keep  <= n_internal_2.keep;
-        internal_2.last  <= n_internal_2.last;
-        internal_2.valid <= n_internal_2.valid;
+        mux_out.data  <= n_mux_out.data;
+        mux_out.keep  <= n_mux_out.keep;
+        mux_out.last  <= n_mux_out.last;
+        mux_out.valid <= n_mux_out.valid;
     end
 end
 
 always_comb begin
-    internal_1_ready = '1;
+    // If we wait for all last signals, the inputs where we have seen the last belong to the next 
+    // stream and cannot yet be consumed
+    if (LAST_HANDLING == WAIT_ALL) begin
+        mux_in_ready = ~last_seen;
+    end else begin
+        mux_in_ready = '1;
+    end
 
-    n_last_seen_2 = last_seen_2;
+    n_last_seen = last_seen;
 
-    n_internal_2.data  = internal_2.data;
-    n_internal_2.keep  = 1'b0;
-    n_internal_2.last  = 1'b0;
-    n_internal_2.valid = 1'b0; 
+    n_mux_out.data  = mux_out.data;
+    n_mux_out.keep  = 1'b0;
+    n_mux_out.last  = 1'b0;
+    n_mux_out.valid = 1'b0; 
 
-    if (internal_2.ready) begin
-        n_internal_2.data  = selected.data;
-        n_internal_2.keep  = selected.keep;
-        n_internal_2.last  = selected.last;
-        n_internal_2.valid = selected.valid; 
+    if (mux_out.ready) begin
+        n_mux_out.data  = selected.data;
+        n_mux_out.keep  = selected.keep;
+        n_mux_out.last  = selected.last;
+        n_mux_out.valid = selected.valid; 
 
         for (int i = 0; i < NUM_INPUTS; i++) begin
             if (tags[i] && !is_selected[i]) begin
-                internal_1_ready[i] = 1'b0; // Stall element
+                mux_in_ready[i] = 1'b0; // Stall element
             end
-            if (LAST_HANDLING == WAIT_ALL && internal_1_last[i] && (is_selected[i] || (internal_1_valid[i] && !tags[i]))) begin
-                n_last_seen_2[i] = 1'b1; // Mark last seen
+            if (LAST_HANDLING == WAIT_ALL && mux_in_last[i] && (is_selected[i] || (mux_in_valid[i] && !tags[i]))) begin
+                n_last_seen[i] = 1'b1; // Mark last seen
             end
         end
 
         // We have seen all last signals or the last element is processed in this clock cycle
-        if (LAST_HANDLING == WAIT_ALL && (&last_seen_2 || ($onehot(tags) && &(last_seen_2 | internal_1_last)))) begin 
-            n_last_seen_2      = '0;
-            n_internal_2.last  = 1'b1;
-            n_internal_2.valid = 1'b1;
+        if (LAST_HANDLING == WAIT_ALL && (&last_seen || ($onehot0(tags) && &(last_seen | (mux_in_valid & mux_in_last))))) begin 
+            n_last_seen     = '0;
+            n_mux_out.last  = 1'b1;
+            n_mux_out.valid = 1'b1;
         end
     end else begin // Not ready => Hold signals
         for (int i = 0; i < NUM_INPUTS; i++) begin
             if (tags[i]) begin
-                internal_1_ready[i] = 1'b0;
+                mux_in_ready[i] = 1'b0;
             end else begin
-                if (LAST_HANDLING == WAIT_ALL && internal_1_valid[i] && internal_1_last[i]) begin
-                    n_last_seen_2[i] = 1'b1;
+                if (LAST_HANDLING == WAIT_ALL && mux_in_valid[i] && mux_in_last[i]) begin
+                    n_last_seen[i] = 1'b1;
                 end
             end
         end
 
-        n_internal_2.keep  = internal_2.keep;
-        n_internal_2.last  = internal_2.last;
-        n_internal_2.valid = internal_2.valid;
+        n_mux_out.keep  = mux_out.keep;
+        n_mux_out.last  = mux_out.last;
+        n_mux_out.valid = mux_out.valid;
     end 
 end
 
-DataSkidBuffer #(data_t) inst_output_buffer (.clk(clk), .rst_n(reset_synced), .in(internal_2), .out(out));
+DataSkidBuffer #(data_t) inst_output_buffer (.clk(clk), .rst_n(reset_synced), .in(mux_out), .out(out));
 
 endmodule
