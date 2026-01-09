c10d0816ede028fdcf9a1ea5b65943d04702bee2
Luca <luca@teapot.ovh>
2026-01-09 17:57:30 +0100

    use std::shared_ptr in BufferDestructor

--- software/src/libstf/buffer.hpp
+++ software/src/libstf/buffer.hpp
@@ -7,25 +7,28 @@
 namespace libstf {
 
 struct Buffer {
-    void  *ptr;      // The address at which the buffer begins
-    size_t size;     // The actual size of the buffer
-    size_t capacity; // The total capacity of the buffer
+  void *ptr;       // The address at which the buffer begins
+  size_t size;     // The actual size of the buffer
+  size_t capacity; // The total capacity of the buffer
 };
 
-// Deleter struct for allocations that is used to clean up the memory that we pass as a shared_ptr.
+// Deleter struct for allocations that is used to clean up the memory that we
+// pass as a shared_ptr.
 struct BufferDeleter {
-    MemoryPool &memory_pool;
+  std::shared_ptr<MemoryPool> memory_pool;
 
-    BufferDeleter(MemoryPool &memory_pool) : memory_pool(memory_pool) {}
+  BufferDeleter(std::shared_ptr<MemoryPool> memory_pool)
+      : memory_pool(memory_pool) {}
 
-    void operator()(Buffer const *buffer) const {
-        // First: free the allocation the struct manages
-        memory_pool.free(buffer->ptr, buffer->capacity);
-        // Then: free the struct itself!
-        delete buffer;
-    }    
+  void operator()(Buffer const *buffer) const {
+    // First: free the allocation the struct manages
+    memory_pool->free(buffer->ptr, buffer->capacity);
+    // Then: free the struct itself!
+    delete buffer;
+  }
 };
 
-std::shared_ptr<Buffer> make_buffer(MemoryPool &memory_pool, void *ptr, size_t size, size_t capacity);
+std::shared_ptr<Buffer> make_buffer(std::shared_ptr<MemoryPool> memory_pool,
+                                    void *ptr, size_t size, size_t capacity);
 
 } // namespace libstf
